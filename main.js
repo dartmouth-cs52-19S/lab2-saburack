8f430178658c714346294637afd833e9800948b3
Sami BUrack <samiburack@gmail.com>
2019-04-07 22:18:34 -0400

    major headway on json

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
+$.getJSON("data.json", function(data) {
+  $('#quiz-title').append(`<h1>${data.quiz_title}</h1>`);
+
+  data.questions.forEach(question => {
+    var current_question = $(`<div class="question"></div>`);
+    current_question.append(`<div class="q-header">
+                            <h3 class="question_name">${question.question_name}</h3>
+                            <img class="question_img_url" src=${question.question_img_url}></div>`)
+
+    var current_choices = $(`<div class="choices"></div>`);
+    question.choices.forEach(choice => {
+
+      var current_label = $(`<label></label>`)
+      current_label.append(`<input type="radio" name=${question.name} value=${choice.value}>`);
+    
+      var curr_choice = $(`<div class="choice"></div>`)
+      if (choice.img) {
+        curr_choice.append(`<img src=${choice.img} class="img-with-border"/>`);
+      }
+      if (choice.caption) {
+        curr_choice.append(`<p class="caption">${choice.caption}</p>`);
+      }
+      current_label.append(curr_choice);
+      current_choices.append(current_label);
+      
+    });
+
+    current_question.append(current_choices);
+    $('.questions').append(current_question);
+  }); 
+
+  $('#submit').html(data.submit_button);
+});
+
 var descriptions = {
     david: "you're not afraid to tell it like it is",
     alexis: "you're a social butterfly, super fashionable, and are always up for anything",
@@ -5,14 +39,14 @@ var descriptions = {
     moira: "you love reliving your glory days and overpronouncing words",
     "uh oh!": "please go back and answer all the questions"
 }
-var numberOfQuestions = 5; 
+
+var winner = ""; // need this to be global 
 
 $('#submit').on('click', function(e) {
     // gather all checked radio-button values
     var choices = $("input[type='radio']:checked").map(function(i, radio) {
       return $(radio).val();
     }).toArray();
-
     
     // used this for help on writing js functions
     //appendto.com/2016/10/finding-the-most-frequent-string-in-a-javascript-array/
@@ -27,7 +61,6 @@ $('#submit').on('click', function(e) {
     };
 
     max = 0;
-    winner = "";
     Object.keys(frequencies).forEach(function(key)  {
         if (frequencies[key] > max) {
             max = frequencies[key];
@@ -35,12 +68,19 @@ $('#submit').on('click', function(e) {
         }
     })
 
-    if (choices.length < numberOfQuestions) {
-        winner = "uh oh!"; 
-        document.getElementById('congrats').innerHTML = "";
-    }
-    document.getElementById('whoami').innerHTML = winner; 
-    document.getElementById('whoami-description').innerHTML = descriptions[winner]; 
+    $.getJSON("data.json", function(data) {
+      console.log(winner);
+      var current_outcome;
+      if (data.questions.length < data.number_of_questions) {
+        current_outcome= $(`<p id="error">${data.error}</p>`);
+      } else {
+        current_outcome = $(`<p id="congrats">${data.congrats}</p>
+                              <p id="whoami">${winner}</p>
+                              <p id="whoami-description">${data.outcomes[winner].text}</p>`);
+      }
+      $('.result-content').append(current_outcome);
+    }); 
+  
 });
 
 // all from https://www.w3schools.com/howto/howto_css_modals.asp
